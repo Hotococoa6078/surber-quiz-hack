2cd7c376eeb14499aca1e31bb37baf56a714e02c
unknown <student@oyamanet.local>
2020-11-30 13:49:08 +0900

    키워드업데이트

--- surber(up 15level).py
+++ surber(up 15level).py
@@ -16,7 +16,8 @@ def fix(a):
     e = e.replace('에헤', '왘').replace('녀ㅠㄷㄱ','왘').replace('Ｓｕｒｂｅｒ','쾅')
     e = e.replace('ㅣㅏ','왘').replace('얃ㅇ', '왘').replace('genius', '왘')
     e = e.replace('천재', '왘').replace('ㅔ', '쾅').replace('슥','쾅')
-    e = e.replace('-수-', '쾅').replace('바부', '쾅')
+    e = e.replace('-수-', '쾅').replace('바부', '쾅').replace('긋','왘')
+    e = e.replace('ㄴㅁㅇ', '쾅').replace('밥부', '쾅')
 
 
     e = e.replace(' ', '').replace(":", "")
