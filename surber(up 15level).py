61936e48efbb3bf1585339bb3c960cc0bafe64ee
unknown <student@oyamanet.local>
2020-11-30 14:00:23 +0900

    키워드업데이트

--- surber(up 15level).py
+++ surber(up 15level).py
@@ -20,7 +20,7 @@ def fix(a):
     e = e.replace('ㄴㅁㅇ', '쾅').replace('밥부', '쾅').replace('과부하', '쾅')
     e = e.replace('das', '왘').replace('knalb', '왘').replace('버스', '왘')
     e = e.replace(':desktop:','쾅').replace('obab', '왘')
-    e = e.replace('potksed','왘').replace('zhkd','쾅').replace('blank', '쾅
+    e = e.replace('potksed','왘').replace('zhkd','쾅').replace('blank', '쾅')
 
 
     e = e.replace(' ', '').replace(":", "")
